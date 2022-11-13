5938094a115b5db5731b4c438e5c641cbfaebe28
Gerardo Martínez <gerardomt@ciencias.unam.mx>
2022-11-13 15:09:31 -0600

    Modifications of color scheme and general style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+        animation: {
+        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite;',
+      }
+    },
+    colors: {
+        'space': '#3F4A59',
+        'tea-green': '#DBF7CB',
+        'sea-green': '#A3E3C6',
+        'green-sheen': '#64B5B0',
+    }
   },
   plugins: [],
 }
